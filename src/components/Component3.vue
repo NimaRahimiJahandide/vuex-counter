4027fb139b21cd5fd2f30a1484ef6d6b5de738fe
Nima <nimarahimijahandide@gmail.com>
2023-04-09 09:51:32 +0330

    vuex-counter

---
+++ src/components/Component3.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Component4 />
+        <History /> 
+    </div>
+</template>
+
+<script>
+import Component4 from './Component4'
+import History from './History.vue'
+    export default {
+        components: {
+            Component4,
+            History
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
